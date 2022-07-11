d123a2e35e8872622c59fbb06e560c67a9ac9bb2
Bruno Bulnes <bulnes.bruno@gmail.com>
2022-07-10 23:10:50 -0300

    feat: using hydrate method to enable ssr

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.hydrateRoot(document.getElementById('root'));
-root.render(
+ReactDOM.hydrateRoot(
+  document.getElementById('root'), 
   <React.StrictMode>
     <App />
   </React.StrictMode>
